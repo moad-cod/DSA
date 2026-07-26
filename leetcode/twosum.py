8a0ad673373bf06671967448d5851f8307246fb8
Mouad El Baz <elbaz.ouad1249@gmail.com>
2026-07-26 03:59:20 +0100

    leetcode(twosum): O(n) time complexity

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -10,8 +10,28 @@ class Solution(object):
                     return [i, j]
         
         return []
+    
+    # # Hash Map
+    def twoSum_HM(self, nums, target):
+        if len(nums) < 2:
+            return []
+        
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            
+            complement = target - num
+            
+            if complement in seen:
+                return [seen[complement], i]
+        
+            seen[num] = i
+            
+        return []
 
-nums = [2,7,11,15]
+nums = [2,7,11,15, 4, 5]
 target = 9
 
 print(Solution().twoSum(nums, target))
+print(Solution().twoSum_HM(nums, target))
+
