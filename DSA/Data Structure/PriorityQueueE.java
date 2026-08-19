a6454765262b1070a1931eb606d6d8d6c7cfd38f
Mouad El Baz <elbaz.ouad1249@gmail.com>
2026-08-19 19:06:57 +0100

    feat: implement Priority Queue data structure in Java

---
+++ DSA/Data Structure/PriorityQueueE.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class PriorityQueueE<T> {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        // PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); For reverse order priority
+
+        queue.add(30);
+        queue.add(10);
+        queue.add(20);
+
+        while(!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+}
